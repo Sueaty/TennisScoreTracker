f9837a9833f39b21cb637dae5a8cb313dc04a483
sueaty <suejeong97@naver.com>
2025-10-05 23:22:10 +0900

    Initial Commit

---
+++ TennisScoreTracker Watch App/TennisScoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisScoreTrackerApp.swift
+//  TennisScoreTracker Watch App
+//
+//  Created by 조수정 on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TennisScoreTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TennisScoreTracker Watch AppTests/TennisScoreTracker_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TennisScoreTracker_Watch_AppTests.swift
+//  TennisScoreTracker Watch AppTests
+//
+//  Created by 조수정 on 10/5/25.
+//
+
+import Testing
+@testable import TennisScoreTracker_Watch_App
+
+struct TennisScoreTracker_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
